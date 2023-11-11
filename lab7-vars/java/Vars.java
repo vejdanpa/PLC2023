b94132bae3d421cd99981e5e8106dc0573826402
Arezoo Vejdanparast <vejdana2@aston.ac.uk>
2023-11-11 20:48:19 +0000

    Updated lab7

--- lab7-vars/java/Vars.java
+++ lab7-vars/java/Vars.java
@@ -1,12 +1,5 @@
-import java.util.Collections;
-import java.util.Scanner;
-import java.util.List;
-import java.util.ArrayList;
-import java.util.InputMismatchException;
-
 public class Vars
 {
-
 
     private static class A { public int a_c; }
     private static class B extends A { public int b_c; }
@@ -26,17 +19,5 @@ public class Vars
         }
 
         // System.out.println("" + i); // ERROR, scope of i ends on the above }
-
-        for(int i = 0; i < 2; i++){
-            // for(int i = 0; i < 3; i++){ // error: duplicate variable
-            //     System.out.println("hi!");
-            // }
-            for(int j = 0; j < 3; j++){
-                System.out.println("hi!");
-            }
-            for(int j = 0; j < 3; j++){
-                System.out.println("hi!");
-            }
-        }
     }
 }
